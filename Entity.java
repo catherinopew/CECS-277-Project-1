56b320079a43419648f49d4aa3f423c781fa10bd
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-03 16:44:51 -0700

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,42 @@
+public class Entity {
+    private String name;
+    private int maxHp;
+    private int hp;
+
+    public Entity(String n, int mHp) {
+        name = n;
+        mHp = maxHp;
+    }
+
+    //abstract String attack(Entity e)
+    //haven't learned this yet?
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return hp;
+    }
+
+    public int getMaxHP() {
+        return maxHp;
+    }
+
+    public void heal(int h) {
+        int heal = 0;
+        heal = hp + h;
+    }
+
+    public void takeDamage(int h) {
+        int damage = 0;
+        damage = hp - h;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        //figure out what you need here
+        return s;
+    }
+}
