de7de629a5b8b90da375ae0289958fcb64df3367
cxr1541 <72367908+cxr1541@users.noreply.github.com>
2020-10-05 17:01:49 -0700

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,76 @@
+
+public class Point {
+
+
+	//instance variable
+	private int x;
+	private int y;
+	
+	//constructor
+	public Point() {
+		x = 0;
+		y = 0;
+	}
+	
+	public Point(int xVal, int yVal) { //overloaded constructor
+		this.x = xVal;
+		this.y = yVal;
+	}
+	
+	
+	public void translate(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public int gety() {
+		return y;
+	}
+	
+	public void move(int xVal, int yVal) {
+		this.x = xVal;
+		this.y = yVal;
+	}
+	
+	public void setLoc(Point p) {
+		this.x = p.x;
+		this.y = p.y;
+	}
+	
+	/*
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return "("+x+","+y+")";
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
