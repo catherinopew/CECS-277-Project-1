bf9257a2a3632f7ea951a47923e0f91f7f6d2e61
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-10 19:13:49 -0700

    javadocs

--- Item.java
+++ Item.java
@@ -1,10 +1,18 @@
+/** Item class to represent an item */
 public class Item {
+    /** A private instance variable that is the name of the item */
     private String name;
 
+    /** Constructs an item
+     * @param n the item name
+     */
     public Item(String n) {
         name = n;
     }
 
+    /** Returns the name of the item
+     * @return String the item name
+     */
     public String getName() {
         return name;
     }
