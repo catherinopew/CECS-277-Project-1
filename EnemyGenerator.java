4ea08fbbdf331bf11a1edc89daf3d425dac934f8
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-10 17:13:42 -0700

    javadocs

--- EnemyGenerator.java
+++ EnemyGenerator.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/** Class EnemyGenerator that generates an enemy */
 public class EnemyGenerator {
+    /** A private ArrayList enemyList of type Enemy */
     private ArrayList <Enemy> enemyList = new ArrayList <Enemy> ();
+    /** A private instance variable ig of type ItemGenerator */
     private ItemGenerator ig;
 
+    /** Reads from the EnemyList.txt file 
+     * and adds each enemy to the ArrayList
+     * @param ig item to be generated 
+     */
     public EnemyGenerator(ItemGenerator ig) {
         try {
             Scanner read = new Scanner(new File("EnemyList.txt"));
@@ -15,7 +22,7 @@ public class EnemyGenerator {
                 String line = read.nextLine();
                 String [] token = line.split(",");
                 enemyList.add(new Enemy(token[0], 
-                Integer.parseInt(token[1]), ig)); 
+                Integer.parseInt(token[1]), ig.generateItem())); 
             }
             read.close();
         } 
@@ -24,6 +31,11 @@ public class EnemyGenerator {
         }
     }
 
+    /**
+     * Randomly generates an enemy from the ArrayList
+     * @param level the level in which the game is on
+     * @return
+     */
     public Enemy generateEnemy(int level) {
         int random = 0;
         random = (int)(Math.random() * enemyList.size());
