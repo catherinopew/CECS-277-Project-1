892cff56335a132378bbf34834ed6896f5ce878a
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-11 13:30:36 -0700

    in progress

--- Map.java
+++ Map.java
@@ -1,68 +1,83 @@
-import java.io.*;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class Map{
+public class Map {
   //private instance var
   private char [][] map = new char [5][5];
 
-  /*private boolean [][] revealed = new boolean [5][];*/
+  private boolean [][] revealed = new boolean [][]{};
 
   //Constructor 
-  Map(){
-    int character_position = map[2][0];
+  public Map() {
+    char character_position = map[2][0];
     int mapNum = 1;
   }
-  public void loadMap(int mapNum){
-    try{
-      /* redundant... map should be displayed in diplayma() */
-      if (mapNum == 1){  
-        Scanner read = new Scanner (new File("Map1.txt"));
-        
-        while (read.hasNext()){
-          String line = read.nextLine();
-          System.out.println(line);
-       
-        }
-        read.close();
-       
-      }
-      
-      else if (mapNum == 2){
-        Scanner read = new Scanner (new File("Map2.txt"));
-        
-        while (read.hasNext()){
-          String line = read.nextLine();
-          System.out.println(line);
-        }
-        read.close();
-      }
-      else if (mapNum == 3){
-        Scanner read = new Scanner (new File("Map1.txt"));
-        
-        while (read.hasNext()){
-          String line = read.nextLine();
-          System.out.println(line);
-        }
 
-        read.close();
-      }
-      else if (mapNum == 4){
-        Scanner read = new Scanner (new File("Map2.txt"));
-        while (read.hasNext()){
-          String line = read.nextLine();
-          System.out.println(line);
-        }
-        read.close();
-      }
-      
-      
-    } catch (FileNotFoundException fnf){
-      System.out.print("NO FILE FOUND.");
+  public void loadMap(int mapNum) {
+    if(mapNum == 1 || mapNum == 4 || mapNum == 7) {
+      displayMap(findStart());
+    }
+    else if(mapNum == 2 || mapNum == 5 || mapNum == 8) {
+      displayMap(findStart());
+    }
+    else {
+      displayMap(findStart());
     }
   }
-  public char getCharAtLoc(point p){
-    character_position = character_position.p;
-    return character_position;
+
+  public char getCharAtLoc(Point p) {
+    return map[p.getX()][p.getY()];
   }
-  
+
+  public void displayMap(Point p) {
+    try {
+      if (p.getX() == 2 && p.getY() == 0) {
+        Scanner read = new Scanner(new File("Map1.txt"));
+        while (read.hasNext()) {
+          String line = read.nextLine();
+          System.out.println(line);
+        }
+        read.close();
+      }
+      else if (p.getX() == 4 && p.getY() == 4) {
+        Scanner read = new Scanner(new File("Map2.txt"));
+        while (read.hasNext()) {
+          String line = read.nextLine();
+          System.out.println(line);
+        }
+        read.close();
+      }
+      else {
+        Scanner read = new Scanner(new File("Map3.txt"));
+        while (read.hasNext()) {
+          String line = read.nextLine();
+          System.out.println(line);
+        }
+        read.close();
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File Not Found - place in project folder");
+    }
+  }
+
+  public Point findStart() {
+    Point start = new Point();
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; i < 5; j++) {
+        if (map [i][j] == 's') {
+          start.setLocation(i, j);
+        }
+      }
+    }
+    return start;
+  }
+
+  public void reveal(Point p) {
+    revealed [p.getX()][p.getY()] = true;
+  }
+
+  public void removeCharAtLoc(Point p) {
+    map[p.getX()][p.getY()] = '\0';
+  }
 }
