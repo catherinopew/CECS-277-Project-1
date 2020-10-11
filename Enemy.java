de47255036346add7baa56517e9fcbd931b92c00
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-10 17:05:04 -0700

    javadocs

--- Enemy.java
+++ Enemy.java
@@ -1,11 +1,21 @@
+/** Enemy class that extends Entity class */
 public class Enemy extends Entity {
+    /** A private instance variable item of type Item */
     private Item item;
     
+    /** Constructs an enemy. 
+     * @param n the enemy name
+     * @param mHp the max hit points
+     * @param i the item
+     */
     public Enemy(String n, int mHp, Item i) {
         super(n, mHp);
         item = i;
     }
 
+    /** Returns an item.
+     * @return the item
+     */
     public Item getItem() {
         return item;
     }
