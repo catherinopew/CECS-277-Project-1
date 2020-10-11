5f367166b63e46ba487149e59ddab88843151320
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-10 19:51:04 -0700

    added attack method and javadocs

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 /** Enemy class that extends Entity class */
 public class Enemy extends Entity {
     /** A private instance variable item of type Item */
@@ -15,22 +14,22 @@ public class Enemy extends Entity {
         item = i;
     }
 
+    /** Returns an item
+     * @return the item
+     */
+    public Item getItem() {
+        return item;
+    }
+
     /** Attacks the hero for a random amount of damage
      * @param e the hero who is being attacked
      */
     @Override
     public String attack(Entity e) {
         Random rand = new Random();
-        int damage = rand.nextInt(15) + 1;
+        int damage = rand.nextInt(10) + 1;
         e.takeDamage(damage);
         return getName() + "attacks" + e.getName() 
         + "for" + damage + "damage.";
-    }
-
-    /** Returns an item
-     * @return the item
-     */
-    public Item getItem() {
-        return item;
     }
 }
