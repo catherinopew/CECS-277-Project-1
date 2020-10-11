534419c7ec3c206aeba83b1853fec40a52f95315
nut <71743743+catherinopew@users.noreply.github.com>
2020-10-10 19:31:21 -0700

    in progress

--- MagicalEnemy.java
+++ MagicalEnemy.java
@@ -1,14 +1,15 @@
+import java.util.Random;
 public class MagicalEnemy extends Enemy {
     public MagicalEnemy(String n, int mHp, Item i) {
         super(n, mHp, i);
     }
 
+    @Override
     public String attack(Entity e) {
-        int damage = 0;
-        if (e.getHP() != 0) {
-            damage = (int)(Math.random() * e.getHP());
-            e.takeDamage(damage);
-        }
-        return e.toString();
+        Random rand = new Random();
+        int damage = rand.nextInt(15) + 1;
+        e.takeDamage(damage);
+        return getName() + "attacks" + e.getName() 
+        + "for" + damage + "damage.";
     }
 }
