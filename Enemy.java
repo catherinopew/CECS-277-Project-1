e5b2fbe1c3ba84d5e3f8adfec9b4d7c3ef802a7f
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-15 16:29:58 -0700

    edited javadocs & changed damage amt

--- Enemy.java
+++ Enemy.java
@@ -2,21 +2,21 @@ import java.util.Random;
 
 /** Enemy class that extends Entity class */
 public class Enemy extends Entity {
-    /** A private instance variable item of type Item */
+    /** An enemy's item of type Item */
     private Item item;
     
     /** Constructs an enemy 
      * @param n the enemy name
-     * @param mHp the max hit points
-     * @param i the item
+     * @param mHp the enemy's max hit points
+     * @param i the enemy's item
      */
     public Enemy(String n, int mHp, Item i) {
         super(n, mHp);
         item = i;
     }
 
-    /** Returns an item
-     * @return the item
+    /** Retrieves an item
+     * @return the enemy's item
      */
     public Item getItem() {
         return item;
@@ -28,7 +28,7 @@ public class Enemy extends Entity {
     @Override
     public String attack(Entity e) {
         Random rand = new Random();
-        int damage = rand.nextInt(10) + 1;
+        int damage = rand.nextInt(7) + 1;
         e.takeDamage(damage);
         return getName() + "attacks" + e.getName() 
         + "for" + damage + "damage.";
