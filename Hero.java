885a04921ece7ab50cc5cdf950f9bdb796d3845e
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-15 17:39:04 -0700

    added javadocs & fixed Hero.java
    initialized hero's location and fixed hero translation in NESW directions

--- Hero.java
+++ Hero.java
@@ -1,44 +1,55 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/** Hero class that represents a hero */
 public class Hero extends Entity implements Magical {
+    /** An ArrayList of items that stores the hero's current items */
     private ArrayList <Item> items = new ArrayList <Item> ();
+    /** The map that the hero is on */
     private Map map; 
+    /** The location of the hero */
     private Point location;
 
+    /** Constructs a hero
+     * @param n the hero's name
+     * @param m the map that the hero is currently on
+     */
     public Hero (String n, Map m) {
         super(n, 25);
         map = m;
+        location = map.findStart();
     }
 
-    @Override
-    public String attack(Entity e) {
-        Random rand = new Random();
-        int damage = rand.nextInt(5) + 1;
-        e.takeDamage(damage);
-
-        return getName() + "attacks" + e.getName() 
-        + "for" + damage + "damage.";
-    }
-
+    /** Displays the hero's current hp out of max hp 
+     * @return String the hero's current hp out of max hp
+     */
     @Override
     public String toString() {
-        return super.toString(); //prob this but idk
+        return super.toString(); 
     }
 
-    //@Override?
+    /** Displays the hero's current inventory
+     * @return String a list of the hero's inventory
+     */
     public String itemsToString() {
         String itemString = "Inventory: \n";
         for (int i = 0; i < items.size(); i++) {
-            itemString += i + ". " + items.get(i) + "\n";
+            itemString += (i + 1) + ". " + items.get(i).getName() + "\n";
         }
         return itemString;
     }
 
+    /** Retrieves the number of items in the hero's inventory
+     * @return int the number of items in the inventory
+     */
     public int getNumItems() {
         return items.size();
     }
 
+    /** Picks up an item if the inventory isn't full
+     * @param i the item being picked up
+     * @return boolean true if the item is picked up, false otherwise
+     */
     public boolean pickUpItem(Item i) {
         boolean pickUp = false;
         if (items.size() < 5) {
@@ -48,15 +59,22 @@ public class Hero extends Entity implements Magical {
         return pickUp;
     }
 
+    /** The hero drinks the potion */
     public void drinkPotion() {
-        int potion = 25;
-        heal(potion);
+        heal(getMaxHP());
     }
 
+    /** Drops an item if the inventory is full and
+     * hero wants to pick up a new item
+     * @param index the index of the item to be dropped
+     */
     public void dropItem(int index) {
         items.remove(index);
     }
 
+    /** Determines whether the hero has a potion in their inventory
+     * @return boolean true if hero has a potion, false otherwise
+     */
     public boolean hasPotion() {
         boolean potion = false;
         for (int i = 0; i < items.size(); i++) {
@@ -67,31 +85,88 @@ public class Hero extends Entity implements Magical {
         return potion;
     }
 
+    /** Retrieves the location of the hero
+     * @return Point the coordinates of the hero's location
+     */
     public Point getLocation() {
         return location;
     }
 
+    /** Moves the hero North
+     * and reveals the hero's location if hero isn't moved out of bounds
+     * @return char the character at the hero's location on the map
+     */
     public char goNorth() {
-        location.translate(0, 1);    
-        return map.getCharAtLoc(location);
+        if (location.getX() == 0) {
+            return map.getCharAtLoc(location);
+        }
+        else {
+            location.translate(-1, 0);  
+            map.reveal(location);
+            return map.getCharAtLoc(location);
+        }
     }
-
+    
+    /** Moves the hero South
+     * and reveals the hero's location if hero isn't moved out of bounds
+     * @return char the character at the hero's location on the map
+     */
     public char goSouth() {
-        location.translate(0, -1);    
-        return map.getCharAtLoc(location);
+        if (location.getX() == 4) {
+            return map.getCharAtLoc(location);
+        }
+        else {
+            location.translate(1, 0);  
+            map.reveal(location);
+            return map.getCharAtLoc(location);
+        }
     }
 
+    /** Moves the hero East
+     * and reveals the hero's location if hero isn't moved out of bounds
+     * @return char the character at the hero's location on the map
+     */
     public char goEast() {
-        location.translate(1,0);    
-        return map.getCharAtLoc(location);
+        if (location.getY() == 4) {
+            return map.getCharAtLoc(location);
+        }
+        else {
+            location.translate(0, 1);  
+            map.reveal(location);
+            return map.getCharAtLoc(location);
+        }
     }
 
+    /** Moves the hero West
+     * and reveals the hero's location if hero isn't moved out of bounds
+     * @return char the character at the hero's location on the map
+     */
     public char goWest() {
-        location.translate(-1,0);    
-        return map.getCharAtLoc(location);
+        if (location.getX() == 0) {
+            return map.getCharAtLoc(location);
+        }
+        else {
+            location.translate(0, -1);  
+            map.reveal(location);
+            return map.getCharAtLoc(location);
+        }
     }
 
-    /** Method to make the hero use a magic missile 
+    /** Performs a physical attack on the enemy
+     * @param e the enemy being attacked
+     * @return String the attack message
+     */
+    @Override
+    public String attack(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(5) + 1;
+        e.takeDamage(damage);
+
+        return getName() + "attacks" + e.getName() 
+        + "for" + damage + "damage.";
+    }
+
+    /** Uses a magic missile on the enemy
      * @param e the enemy being attacked
      * @return String the attack message
      */
@@ -105,7 +180,7 @@ public class Hero extends Entity implements Magical {
         + damage + "damage.";
     }
 
-    /** Method to make the hero use a fireball
+    /** Uses a fireball on the enemy
      * @param e the enemy being attacked
      * @return String the attack message
      */
@@ -119,7 +194,7 @@ public class Hero extends Entity implements Magical {
         + damage + "damage.";
     }
 
-    /** Method to make the hero use thunderclap 
+    /** Uses thunderclap on the enemy
      * @param e the enemy being attacked
      * @return String the attack message
      */
