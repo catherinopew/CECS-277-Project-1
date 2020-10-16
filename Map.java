e868469848d3b1838bf50e07ac42b1c23bedaf27
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-15 20:35:44 -0700

    added javadocs & fixed Map.java
    mostly done but might need a bit of tweaking after running Main.java

--- Map.java
+++ Map.java
@@ -2,69 +2,78 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/** Map class that represents the map that the hero is on */
 public class Map {
-  //private instance var
-  private char [][] map = new char [5][5];
+  /** A 2D array of chracters of the map */
+  private char [][] map;
+  /** A 2D array of boolean values */
+  private boolean [][] revealed;
 
-  private boolean [][] revealed = new boolean [5][5];
-
-  //Constructor 
+  /** Constructs a 5 x 5 map and 
+   * Initiliazes all boolean values in revealed to false
+   */
   public Map() {
-    char character_position = map[2][0];
-    int mapNum = 1;
+    map = new char [5][5];
+    revealed = new boolean [5][5];
+    for (int i = 0; i < revealed.length; i++) {
+      for (int j = 0; j < revealed.length; j++) {
+        revealed [i][j] = false;
+      }
+    }
   }
 
+  /** Reads in the appropriate map file given the map number
+   * @param mapNum the map number
+   */
   public void loadMap(int mapNum) {
-    if(mapNum == 1 || mapNum == 4 || mapNum == 7) {
-      displayMap(findStart());
-    }
-    else if(mapNum == 2 || mapNum == 5 || mapNum == 8) {
-      displayMap(findStart());
-    }
-    else {
-      displayMap(findStart());
-    }
-  }
-
-  public char getCharAtLoc(Point p) {
-    return map[p.getX()][p.getY()];
-  }
-
-  public void displayMap(Point p) {
     try {
-      if (p.getX() == 2 && p.getY() == 0) {
-        Scanner read = new Scanner(new File("Map1.txt"));
-        while (read.hasNext()) {
-          String line = read.nextLine();
-          System.out.println(line);
-        }
-        read.close();
+      Scanner read = new Scanner(new File("Map" + mapNum + ".txt"));
+      while (read.hasNext()) {
+        String line = read.nextLine();
+        System.out.println(line);
       }
-      else if (p.getX() == 4 && p.getY() == 4) {
-        Scanner read = new Scanner(new File("Map2.txt"));
-        while (read.hasNext()) {
-          String line = read.nextLine();
-          System.out.println(line);
-        }
-        read.close();
-      }
-      else {
-        Scanner read = new Scanner(new File("Map3.txt"));
-        while (read.hasNext()) {
-          String line = read.nextLine();
-          System.out.println(line);
-        }
-        read.close();
-      }
+      read.close();
     } catch (FileNotFoundException e) {
       System.out.println("File Not Found - place in project folder");
     }
   }
 
+  /** Retrieves the character at a certain location
+   * @param p the point coordinates of the location
+   * @return char the character at the location
+   */
+  public char getCharAtLoc(Point p) {
+    return map[p.getX()][p.getY()];
+  }
+
+  /** Displays the map with the appropriate characters
+   * @param p the point coordinates of the hero's location
+   */
+  public void displayMap(Point p) {
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map.length; j++) {
+        if (revealed[i][j] == true) {
+          System.out.print(map[i][j]);
+        }
+        else if (i == p.getX() && j == p.getY()) {
+          System.out.print('*');
+        }
+        else {
+          System.out.print('x');
+        }
+        //System.out.print(" ");
+      }
+      //System.out.println();
+    }
+  }
+
+  /** Finds the hero's starting point on the map
+   * @return Point the point coordinates of the starting point
+   */
   public Point findStart() {
     Point start = new Point();
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; i < 5; j++) {
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; i < map.length; j++) {
         if (map [i][j] == 's') {
           start.setLocation(i, j);
         }
@@ -73,11 +82,17 @@ public class Map {
     return start;
   }
 
+  /** Reveals a character if the hero has visited that location
+   * @param p the point coordinates of the location
+   */
   public void reveal(Point p) {
     revealed [p.getX()][p.getY()] = true;
   }
 
+  /** Removes a character at a certain location
+   * @param p the point coordinates of the location
+   */
   public void removeCharAtLoc(Point p) {
-    map[p.getX()][p.getY()] = '\0';
+    map[p.getX()][p.getY()] = 0;
   }
 }
