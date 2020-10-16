e7852648be3f54fa8ddac1be9f15570552ecb770
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-15 19:10:18 -0700

    edited javadocs & fixed attack methods
    modified damage amts

--- MagicalEnemy.java
+++ MagicalEnemy.java
@@ -1,19 +1,19 @@
 import java.util.Random;
 
 /** MagicalEnemy class that represents a magical enemy
- * This class extends Enemy and implements Magical
+ * It extends Enemy and implements the Magical interface
  */
 public class MagicalEnemy extends Enemy implements Magical {
     /** Constructs a magical enemy
-     * @param n the name of the enemy
-     * @param mHp the max hit points of the enemy
-     * @param i the item on the enemy
+     * @param n the magical enemy's name
+     * @param mHp the magical enemy's max hit points 
+     * @param i the magical enemy's item
      */
     public MagicalEnemy(String n, int mHp, Item i) {
         super(n, mHp, i);
     }
 
-    /** Randomly chooses an attack to attack the hero
+    /** Randomly chooses a magical attack to attack the hero
      * @param e the hero being attacked
      * @return String the attack message
      */
@@ -32,42 +32,42 @@ public class MagicalEnemy extends Enemy implements Magical {
         }
     }
 
-    /** Method to make the enemy use a magic missile 
+    /** Uses a magic missile on the hero
      * @param e the hero being attacked
      * @return String the attack message
      */
     @Override
     public String magicMissile(Entity e) {
         Random rand = new Random();
-        int damage = rand.nextInt(10) + 1;
+        int damage = rand.nextInt(7) + 1;
         e.takeDamage(damage);
 
         return getName() + "hits" + e.getName() + "with a Magic Missle for"
         + damage + "damage.";
     }
 
-    /** Method to make the enemy use a fireball
+    /** Uses a fireball on the hero
      * @param e the hero being attacked
      * @return String the attack message
      */
     @Override
     public String fireball(Entity e) {
         Random rand = new Random();
-        int damage = rand.nextInt(10) + 1;
+        int damage = rand.nextInt(7) + 1;
         e.takeDamage(damage);
 
         return getName() + "hits" + e.getName() + "with a Fireball for"
         + damage + "damage.";
     }
 
-    /** Method to make the enemy use thunderclap 
+    /** Uses thunderclap on the hero
      * @param e the hero being attacked
      * @return String the attack message
      */
     @Override
     public String thunderclap(Entity e) {
         Random rand = new Random();
-        int damage = rand.nextInt(10) + 1;
+        int damage = rand.nextInt(7) + 1;
         e.takeDamage(damage);
 
         return getName() + "zaps" + e.getName() + "with Thunderclap for"
